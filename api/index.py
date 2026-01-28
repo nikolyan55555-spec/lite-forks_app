56f21641aa147b04d1ddabeacce29b0d2b75bf27
nikolyan55555-spec <niKolyan55555@gmail.com>
2026-01-28 21:00:19 +0300

    Auto update data: 2026-01-28 21:00:19

--- api/index.py
+++ api/index.py
@@ -31,39 +31,9 @@ def handler():
     Основная функция Vercel. 
     Получает запрос, проверяет параметры URL, генерирует и возвращает полный HTML.
     """
-    is_subscribed = False
-    result_html = "" 
 
-    # # 1. Извлекаем параметры из URL-строки запроса
-    # query_string = request.query_string.decode('utf-8')
-    # # request.args в Flask уже парсит query_string, parse_qs не нужен
-    # auth_data = request.args 
+    result_html = "<p>Telegram.</p>"
 
-    # print(auth_data) # Отладочный вывод в логи Vercel
-
-    auth_data = {'id': 5, 'hash': 6}
-
-    # 2. Проверяем авторизацию Telegram
-    if auth_data.get('id') and auth_data.get('hash'):
-        result_html = "<p>Telegram.</p>"
-        # if verify_telegram_signature(auth_data):
-        #     # Проверяем срок годности данных
-        #     # Flask request.args возвращает строки, нужно преобразовать в int
-     
-        #     # user_id = int(auth_data.get('id'))
-        #     # user_info = USERS_DATA.get(user_id)
-        #     # if user_info and user_info['is_subscribed']:
-        #     #     is_subscribed = True
-            
-        #     # # 3. Генерируем контент, соответствующий статусу подписки
-        #     # result_html = create_service_html(is_subscribed=is_subscribed)
-        #     result_html = "<p>Telegram.</p>"
-
-        # else:
-        #     result_html = "<p>Ошибка: Неверная подпись Telegram.</p>"
-    else:
-        result_html = "<p>Ошибка: Нет данных авторизации Telegram.</p>"
-    
     # Flask имеет встроенную функцию render_template_string
     return render_template_string(result_html)
 
