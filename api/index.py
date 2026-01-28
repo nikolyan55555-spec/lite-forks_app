a5a5beba6780112bdb7d59791dc4f8b5d014d967
nikolyan55555-spec <niKolyan55555@gmail.com>
2026-01-28 20:57:35 +0300

    Auto update data: 2026-01-28 20:57:35

--- api/index.py
+++ api/index.py
@@ -4,12 +4,12 @@ from flask import Flask, request, render_template_string
 # Vercel ожидает найти переменную 'app'
 app = Flask(__name__)
 
-# --- ЗАГЛУШКИ ДЛЯ ПРИМЕРА (ЗАМЕНИТЕ НА ВАШУ РЕАЛИЗАЦИЮ) ---
-BOT_TOKEN = "8385615154:AAEwVHr3LcUVkDAL5NiJSImOy2trol_YRp0" 
-USERS_DATA = {
-    # Пример данных пользователя: {user_id: {'is_subscribed': True/False}}
-    12345678: {'is_subscribed': True} 
-}
+# # --- ЗАГЛУШКИ ДЛЯ ПРИМЕРА (ЗАМЕНИТЕ НА ВАШУ РЕАЛИЗАЦИЮ) ---
+# BOT_TOKEN = "8385615154:AAEwVHr3LcUVkDAL5NiJSImOy2trol_YRp0" 
+# USERS_DATA = {
+#     # Пример данных пользователя: {user_id: {'is_subscribed': True/False}}
+#     12345678: {'is_subscribed': True} 
+# }
 
 def verify_telegram_signature(data):
     """(Заглушка) Реализуйте здесь вашу функцию верификации из предыдущего ответа."""
@@ -49,13 +49,14 @@ def handler():
             # Проверяем срок годности данных
             # Flask request.args возвращает строки, нужно преобразовать в int
      
-            user_id = int(auth_data.get('id'))
-            user_info = USERS_DATA.get(user_id)
-            if user_info and user_info['is_subscribed']:
-                is_subscribed = True
+            # user_id = int(auth_data.get('id'))
+            # user_info = USERS_DATA.get(user_id)
+            # if user_info and user_info['is_subscribed']:
+            #     is_subscribed = True
             
-            # 3. Генерируем контент, соответствующий статусу подписки
-            result_html = create_service_html(is_subscribed=is_subscribed)
+            # # 3. Генерируем контент, соответствующий статусу подписки
+            # result_html = create_service_html(is_subscribed=is_subscribed)
+            result_html = "<p>Telegram.</p>"
 
         else:
             result_html = "<p>Ошибка: Неверная подпись Telegram.</p>"
